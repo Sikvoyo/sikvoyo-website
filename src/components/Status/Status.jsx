aeec533b43af6661f330403055f437927e12140d
Sikvoyo <suruhakken@gmail.com>
2022-10-29 19:10:28 +0300

    update

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -4,7 +4,8 @@ const Status = () => {
     return(
         <>
             <div id='status' className='not-affect'>
-                <p>"я не думаю, я ем."</p>
+                <p>UPDATE: Hi! kvvoya here. Unfortunately, due to my personal problems, lack of free time and stuff I am not going to create anything new in near future. So this is why this website doesn't get updated and it will still remain unfinished for some time. I'm really sorry and I hope I will be able to do stuff soon </p>
+                <p>Also I'm working on something but this is a secret :) Don't get too hyped this is on a very VERY early stage</p>
             </div>
         </>
     );
