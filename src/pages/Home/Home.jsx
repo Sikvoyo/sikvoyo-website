bea010d0e1a0f9e727e5c55ab6c544f1b98092ac
Sikvoyo <suruhakken@gmail.com>
2023-01-29 15:54:30 +0300

    its all over

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,12 @@
-import BGTest from "../../components/BGText/BGText"
-import Start from "../../components/Start/Start"
-import IconCircle from "../../components/IconCircle/IconCircle"
-import Logo from "../../components/Logo/Logo"
-import AboutMe from "../../components/AboutMe/AboutMe"
-import Skills from "../../components/Skills/Skills"
-import Footer from "../../components/Footer/Footer"
-import Navbar from '../../components/Navbar/Navbar';
-import Status from "../../components/Status/Status"
-import { DEFAULT_BG_TEXT } from "../../consts"
-import {React, useState } from 'react';
+import {React} from 'react';
+import css from './Home.module.css'
 
 const Home = () => {
-    const [bgText, setBGText] = useState(DEFAULT_BG_TEXT);
 
     return <>
-        <BGTest text={bgText}/>
-        <Navbar hoverChange={setBGText}/>
-        <Start>
-            <IconCircle hoverChange={setBGText}/>
-            <Logo />
-        </Start>
-        <Status />
-        <AboutMe hoverChange={setBGText}/>
-        <Skills hoverChange={setBGText}/>
-        <Footer />
+    <div className={css.container}>
+        <h1 className={css.kvvoya}>kvvoya</h1>
+    </div>
     </>
 }
 
