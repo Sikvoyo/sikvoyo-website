6135513467d780ecaced8079f5d393f5952bb67f
kvvoya <suruhakken@gmail.com>
2023-05-13 17:28:15 +0300

    maddie i love you

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import loadable from '@loadable/component';
 import Funny from './pages/Funny/Funny'
 import './App.css';
+import lovely_img from './img/makesweet-djhszr.gif'
 
 
 const Home = loadable(() => import('./pages/Home/Home'));
@@ -58,11 +59,24 @@ function App() {
         <p>If you are an artist, remember, there is no such thing as "good art" or "bad art". It's just art, it's your art.</p>
         <br/>
         <p>- kvvoya</p>
-      </>}
-      />
-      <Route path='/testing...testing...testing...' element={
+      </>} />
+      <Route path='/announcments/during_jacob' element={
         <>
-          
+          <h1>During Jacob Update</h1>
+          <p>I have a very bad habit. When I have an idea I like, I have troubles with keeping the idea to myself. There has been quite a lot of times when I said smth like "ooh! I am planning to do <b> this thing</b> or <b> that thing. </b> And then I never make it.</p>
+          <p>It's just how my brain works, and I believe I am not the only one who has that habit to say instead of doing. I noticed that and decided to really shut the fuck up and maybe then I'll be able to actually make that idea become real. Like sikvoyo.fun for example. I never said anything about it and here it is! You are on sikvoyo.fun rn. There even used to be something on that website...</p>
+          <p>So I had that idea, what if I try and make an album. And the point of the album would be me learning to make music. I knew album is a hard thing to do and it is quite easy to give up on it. So I tried to not tell anyone about it BUT I COULDN'T. IT'S SO HARD. I COULDN'T DO IT and said that "hey Imma bout to make a during jacob yay!" and then I was like omg no. I'll have to actually do my best to not give up on it.</p>
+          <p>Don't worry DURING JACOB fans I'm not giving up on it. Just the thing is that ideas tend to change. They tend to develop. I came to realize that I want DURING JACOB to be special. To be something unironically good and not just some sort of a joke. I would like to combine electronic music with guitar, with real life sounds and stuff. I'll need quite a lot of time to get decent at this.</p>
+          <p>Sorry. I should not have told anyone about DURING JACOB. I should have taken my time with it for real and announce it only when it would be almost there.</p>
+          <p>I appreciate your patience.</p>
+          <br/>
+          <p>- kvvoya, from Team Cherry</p>
+        </>
+      }/>
+      <Route path='/mmmm' element={
+        <>
+          <h1 className='mmmm'>MADDIE I LOVE YOU! ❤❤❤❤❤❤</h1>
+          <img src={lovely_img} alt="funny gif, shame you can't see it 4 some reason"></img>
         </>
       }/>
       <Route path='*' element={
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,24 @@ const Home = () => {
     return <>
     <div className={css.container}>
         <h1 className={css.kvvoya}>kvvoya</h1>
+        <h4 className={css.kvvoya}>under construction</h4>
+        <h6 className={css.kvvoya}>it won't be finished soon</h6>
+        <ul className='links'>
+            <li><a href='https://www.twitter.com/kvvoya'>Twitter</a></li>
+            <li><a href='https://instagram.com/kvvoya'>Instagram</a></li>
+            <li><a href='https://rateyourmusic.com/~kvvoya'>RateYourMusic</a></li>
+            <li><a href='https://github.com/kvvoya'>GitHub</a></li>
+            <li><a href='https://steamcommunity.com/id/kvvoya'>Steam</a></li>
+            <li><a href='https://open.spotify.com/user/uf54ekf86axg4lvzivm1wedqr'>Spotify</a></li>
+            <li><a href='https://www.last.fm/user/kvvoya'>Last.fm</a></li>
+            <li>kvvoya#2445</li>
+        </ul>
+        <br />
+        <p className={css.pinfo}>Hello! I am kvvoya! I am drain-pilled and i really really love madeline UwU</p>
+        <p className={css.pinfo}>Anyway, I am pretty <strike>normal</strike> silly, like <strike>nothing</strike> everything special. I like doing stuff, but I don't do that so often. One day there will be stuff I would like to share, and this website will be the place for it. The idea for a new website is kinda crazy, so it will take time and energy and won't happen in a near time.</p>
+        <p className={css.pinfo}>I am. You can find me on the links above!</p>
+        <p className={css.pinfo}>I am also working on some of my major projects such as my debut album and secret stuff! There will be information about it once I remake this website (hopefully some time maybe)</p>
+        <p className={css.pinfo}>So, yeah! I hope you're doing well! Uhhhhmmmmm....</p>
     </div>
     </>
 }
